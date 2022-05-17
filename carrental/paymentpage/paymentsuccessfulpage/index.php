c7d733688dcfeaf1242c84a36c6bc2e0e6a6a45e
Anirokii <medalichaabane2017@gmail.com>
2022-05-17 18:29:58 +0100

    first commit

---
+++ carrental/paymentpage/paymentsuccessfulpage/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en-US">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta name="viewport" content="width=device-width"/>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link
+      rel="icon"
+      type="image/png"
+      href="https://d2woyw6vp5g0b2.cloudfront.net/images/favicon_min.png"
+    />
+    <title>Payment Successful</title>
+    <meta name="description" content="Integrated Software is a combination of Sales CRM, Marketing Automation, Support ticketing software, and Custom Success Manager. It's an umbrella for your whole business process to generate leads, to sell faster, to resolve easier, and to scale quicker."/>
+    <meta name="keywords" content="sms marketing, missed call service provider, lead management system, ivr service provider, crm, integrated sales and marketing" />
+    <meta name="author" content="Corefactors, Teleduce, ISM" />
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link href="css/Montserrat.min.css" rel="stylesheet" />
+    <!-- Twitter Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <!-- template main css file -->
+    <link rel="stylesheet" href="css/main.min.css" />
+
+    <!-- <link rel="stylesheet" href="css/seo-thankyou.css" /> -->
+    <link rel="stylesheet" href="css/payment-successful.css" />
+
+    <!--- Font awesome file --->
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css" />
+
+    <!--canonical tag-->
+    <link rel="canonical" href="https://www.corefactors.in/" />
+
+    <!--cookie consent-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+
+    <!-- Template Javascript Files
+    ================================================== -->
+    <!-- jquery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.min.js" async defer></script>
+    <!-- wow js -->
+    <script src="js/wow.min.js" async defer></script>
+    <!-- template main js -->
+    <script src="js/main.min.js" async defer></script>
+
+    <script src="https://wchat.freshchat.com/js/widget.js"></script>
+
+   
+  </head>
+  <body>
+     <section class="thankyou-section">
+       <div class="container">
+         <div class="thankyou-content">
+           <h1>Thank You</h1>
+           <div class="thumbs-up">
+            <img src="images/landingpage/thumbs-up-bg.svg" class="thumbs-up-bg">
+            <img src="images/landingpage/thumbs-up-icon.svg" class="thumbs-up-image">
+           </div>
+           <h2>Payment Done Successfully!</h2>
+         </div>
+       </div>
+     </section>
+
+
+   
+    <script src="js/common.min.js"></script>
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.0/jquery.cookie.js"></script>
+
+    <div class="wrap" style="margin-top:-3px">
+    <span class="decor"></span>
+    <nav>
+        <ul class="primary">
+          <li>
+             <a href="http://localhost/carrental/carrental/paymentpage/index.php"><b><center><h2>B A C K</h2></center></b></a>
+          </li>
+        </ul>
+    </nav>
+    </div>
+
+  </body>
+</html>
